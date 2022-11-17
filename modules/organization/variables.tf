e3c03d7979e8816d63d4f8f2937b3a49bfce1582
Romain Guichard <romain@particule.io>
2022-11-17 08:51:52 +0100

    fix: organization branding

--- modules/organization/variables.tf
+++ modules/organization/variables.tf
@@ -15,7 +15,21 @@ variable "display_name" {
 }
 
 variable "branding" {
-  description = "Branding for the desired organization"
-  type        = map(string)
-  default     = {}
+  description = "Branding for the Auth0 organization"
+  type = list(object({
+    logo_url = string
+    colors = object({
+      primary         = string
+      page_background = string
+    })
+  }))
+  default = []
+}
+
+variable "connections" {
+  description = "Connections configuration for Auth0 organization"
+  type = list(object({
+    connection_id = string
+  }))
+  default = []
 }
